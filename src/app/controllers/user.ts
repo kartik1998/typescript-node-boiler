a8b7fe8e853c84377a4572b812a2d7c45e413ff5
kartik1998 <kartikrawat61@gmail.com>
2021-11-13 15:26:24 +0530

    fix: 🐛 insertUser method

--- src/app/controllers/user.ts
+++ src/app/controllers/user.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import out from '@lib/apiout';
 import * as userService from '@root/app/services/user';
 
-export function insertUser(req: Request, res: Response) {
-  const result = userService.insertUser(req.body.name);
+export async function insertUser(req: Request, res: Response) {
+  const result = await userService.insertUser(req.body.name);
   return out.success(res, out.SUCCESS, result);
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 const DefaultSchema = new mongoose.Schema({}, { strict: false });
-const User = mongoose.model('User', DefaultSchema, 'user');
+const User = mongoose.model('User', DefaultSchema, 'users');
 
 export default User;
